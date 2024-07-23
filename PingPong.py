82caddbcfc177e215738996b400ce501ac6dbf74
Sedat Kilic <36122776+kilicsedat@users.noreply.github.com>
2024-07-23 18:06:30 -0400

    Update PingPong.py

--- PingPong.py
+++ PingPong.py
@@ -24,7 +24,7 @@ score_b = 0
 #paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
-paddle_a.shape("square")
+paddle_a.shape("circle")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
